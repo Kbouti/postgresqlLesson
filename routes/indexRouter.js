660f03bb2cd31946262a0727c564a422d3a079c4
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-08-21 16:09:55 -0700

    Render index.ejs view

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,7 +4,10 @@ const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => {
     console.log("Usernames will be logged here");
-    res.send("Router response sent");
+    // res.send("Router response sent");
+    res.render("../views/index", {title: "PostgreSQL Lesson"});
+
+
   });
 
   indexRouter.get("/kevin", (req, res) => res.send("Kevin is cool"));
