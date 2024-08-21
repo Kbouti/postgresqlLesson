4cd807a8b792b18368f499cdaf112f4162b5933e
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-08-21 16:35:42 -0700

    Reorganize form route to coform to lesson

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -8,15 +8,19 @@ indexRouter.get("/", (req, res) => {
     res.render("../views/index", {title: "PostgreSQL Lesson"});
   });
 
-indexRouter.post("/", (req, res) => {
-    console.log("Form submitted");
-    res.render("../views/index", {title: "PostgreSQL Lesson"});
+
+  indexRouter.get("/new", (req, res) => {
+    console.log("/new get request received");
+    res.render("../views/form", {title: "New user form"});
+  });
+
+
+indexRouter.post("/new", (req, res) => {
+    console.log("/new post request received");
+    // console.log(`req.body.username: ${req.body.username}`);
+    res.render("../views/index", {title: "Form submitted!"});
 })
 
-
-
   indexRouter.get("/kevin", (req, res) => res.send("Kevin is cool"));
-
-
 
   module.exports = indexRouter;
