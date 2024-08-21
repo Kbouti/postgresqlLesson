363c5f8c755c62fa0ab0ae1c1d1461ae85cc2b16
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-08-21 12:49:26 -0700

    Add console.log statement to index route

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("PostgreSQL lesson"));
+app.get("/", (req, res) => {
+  console.log("usernames will be logged here");
+  res.send("PostgreSQL lesson");
+});
 
 // app.get("/new", )
 
-
-
 // const formController = [
-    
+
 // ]
-
-
 
 const PORT = 3000;
 app.listen(PORT);
