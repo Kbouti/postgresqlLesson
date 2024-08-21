b6b2959aba7d477ecfd4cb610ac905a3874c20ec
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-08-21 12:45:31 -0700

    Create controllers and routes folders

--- app.js
+++ app.js
@@ -3,5 +3,15 @@ const app = express();
 
 app.get("/", (req, res) => res.send("PostgreSQL lesson"));
 
+// app.get("/new", )
+
+
+
+// const formController = [
+    
+// ]
+
+
+
 const PORT = 3000;
 app.listen(PORT);
