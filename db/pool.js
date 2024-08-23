85a26b656c9b79a523fd907b52a566950add6a83
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-08-23 09:53:39 -0700

    Access credentials from.env and create db/queries file

--- db/pool.js
+++ db/pool.js
@@ -1,15 +1,13 @@
 const { Pool } = require("pg");
 
-// All of the following properties should be read from environment variables
-// We're hardcoding them here for simplicity
 
 // Sooooo next up create a .env file and store our user and password there?
 
 module.exports = new Pool({
   host: "localhost", // or wherever the db is hosted
-  user: "<role_name>",
+  user: process.env.DB_USER,
   database: "top_users",
-  password: "<role_password>",
+  password: process.env.DB_PASSWORD,
   port: 5432 // The default port
 });
 
