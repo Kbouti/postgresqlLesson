3565da049932a8f5cee16abfc2bb4ca047a06c2b
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-08-23 11:41:59 -0700

    Add username to database on form submit

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,10 +2,6 @@ const { body, validationResult } = require("express-validator");
 
 const db = require("../db/queries");
 
-
-
-// ************************************************************************************************
-
 async function getUsernames(req, res) {
   const usernames = await db.getAllUsernames();
   console.log(`Usernames received: ${usernames ? true : false}`);
@@ -17,35 +13,16 @@ async function getUsernames(req, res) {
   //   res.send("Usernames: " + usernames.map(user => user.username).join(", "));
 }
 
-// Above is copied, I commented out their res.send as we'll log instead of render at this stage. 
-// ************************************************************************************************
-// Below is copied from the tutorial. I'll modify slightly to match what I've made. 
-
-async function createUsernameGet(req, res) {
-  // render the form
-}
-
-async function createUsernamePost(req, res) {
+async function createUsername(req, res) {
   const { username } = req.body;
+  //   Note we could also access username with:
+  // const username = req.body.username;
+
+  console.log(`Creating username: ${username}`);
   await db.insertUsername(username);
-  res.redirect("/");
 }
-
-// ************************************************************************************************
-
-
-const handleFormSubmit = async (req, res) => {
-  console.log(`Form submit event triggered`);
-  const username = req.body.username;
-  console.log(`username: ${username}`);
-
-  // test:
-  console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`);
-};
 
 module.exports = {
-  handleFormSubmit,
   getUsernames,
-  createUsernameGet,
-  createUsernamePost,
+  createUsername,
 };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,10 +4,13 @@ const indexRouter = Router();
 
 const indexController = require("../controllers/indexController");
 
-
 indexRouter.get("/", (req, res) => {
   console.log("Usernames will be logged here");
   const usernames = indexController.getUsernames();
+
+  // *************************************************************************************
+  // We can now use usernames to render them in our view
+  // *************************************************************************************
 
   res.render("../views/index", { title: "PostgreSQL Lesson" });
 });
@@ -19,8 +22,14 @@ indexRouter.get("/new", (req, res) => {
 
 indexRouter.post("/new", (req, res) => {
   console.log("/new post request received");
-  indexController.handleFormSubmit(req, res);
-  res.render("../views/index", { title: "Form submitted!" });
+  //   Add username to database:
+  indexController.createUsername(req, res);
+  //   Log all names to make sure it worked:
+  indexController.getUsernames;
+
+  //   Return home
+  res.redirect("/");
+  //   res.render("../views/index", { title: "Form submitted!" });
 });
 
 indexRouter.get("/kevin", (req, res) => res.send("Kevin is cool"));
