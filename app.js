e0e969eefa1f9ad4d7d76e027773cc0817728f9e
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-08-23 11:31:16 -0700

    Copy db functions from tutorial

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 
-
-
 const indexRouter = require("./routes/indexRouter");
 
 app.use("/", indexRouter);
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,14 +2,37 @@ const { body, validationResult } = require("express-validator");
 
 const db = require("../db/queries");
 
+
+
+// ************************************************************************************************
+
 async function getUsernames(req, res) {
   const usernames = await db.getAllUsernames();
   console.log(`Usernames received: ${usernames ? true : false}`);
-  console.log("Usernames: " + usernames.map(user => user.username).join(", "));
+  console.log(
+    "Usernames: " + usernames.map((user) => user.username).join(", ")
+  );
 
-  return usernames
-//   res.send("Usernames: " + usernames.map(user => user.username).join(", "));
+  return usernames;
+  //   res.send("Usernames: " + usernames.map(user => user.username).join(", "));
 }
+
+// Above is copied, I commented out their res.send as we'll log instead of render at this stage. 
+// ************************************************************************************************
+// Below is copied from the tutorial. I'll modify slightly to match what I've made. 
+
+async function createUsernameGet(req, res) {
+  // render the form
+}
+
+async function createUsernamePost(req, res) {
+  const { username } = req.body;
+  await db.insertUsername(username);
+  res.redirect("/");
+}
+
+// ************************************************************************************************
+
 
 const handleFormSubmit = async (req, res) => {
   console.log(`Form submit event triggered`);
@@ -20,6 +43,9 @@ const handleFormSubmit = async (req, res) => {
   console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`);
 };
 
-module.exports = { 
-    handleFormSubmit,
-     getUsernames };
+module.exports = {
+  handleFormSubmit,
+  getUsernames,
+  createUsernameGet,
+  createUsernamePost,
+};
