c3d4fba1f10c18a3c073a80a0934f7aa64e69c14
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-08-30 08:11:50 -0700

    Save notes

--- db/queries.js
+++ db/queries.js
@@ -11,11 +11,25 @@ async function querySearchResults(searchTerm) {
   console.log(`Performing query for ${searchTerm}`);
 
   const { rows } = await pool.query(
-    `select * from usernames where username like ${searchTerm}`
+    `select * from usernames if exists where username = "${searchTerm}";`
   );
+
+// const { rows } = await pool.query(
+//     `select * from usernames;`
+//   );
+
+
+
   // **************************************************************************************************
   // Next we need to perform the sql search and return our results.
   //   Brush up on sql so we can get what we need out of ^^that
+
+//   We're getting an error because the search results aren't found. Even if we're searching for something that should be found. 
+// We need to: 
+// -Not return an error when our search term isn't found. 
+// -Actually return results when we search for a term that exists
+
+
   // **************************************************************************************************
   if (rows) {
     rows.forEach((row) => console.log(row));
